f86576c7f202cf6b7973396d471b78eec365241a
Alexandre Sauvaget <alexandre.sauvaget44@gmail.com>
2022-10-03 16:35:47 +0200

    style: mise à jour du style

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,8 +1,9 @@
-import create from 'zustand';
-import {nanoid} from 'nanoid';
+import create from 'zustand'
+import {nanoid} from 'nanoid'
 
 const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key))
 const setLocalStorage = (key, value) => window.localStorage.setItem(key, JSON.stringify(value))
+
 
 export const useStore = create((set) => ({
   texture: 'dirt',
@@ -17,30 +18,30 @@ export const useStore = create((set) => ({
           texture: prev.texture
         }
       ]
-    }));
+    }))
   },
   removeCube: (x, y, z) => {
     set((prev) => ({
       cubes: prev.cubes.filter(cube => {
-        const [X, Y, Z] = cube.pos;
-        return X !== x || Y !== y || Z !== z;
+        const [X, Y, Z] = cube.pos
+        return X !== x || Y !== y || Z !== z
       })
 
-    }));
+    }))
   },
   setTexture: (texture) => {
     set(() => ({
       texture
-    }));
+    }))
   },
   saveWorld: () => {
     set((prev) => {
-      setLocalStorage('cubes', prev.cubes);
-    });
+      setLocalStorage('cubes', prev.cubes)
+    })
   },
   resetWorld: () => {
     set(() => ({
       cubes: []
-    }));
-  }
-}));
+    }))
+  },
+}))
